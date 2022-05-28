e7cb4abb5e9dc041238e8670d6f9cc4a681bdd5d
DaCapoStotel <106014492+DaCapoStotel@users.noreply.github.com>
2022-05-28 17:40:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var scroll = this.pageYOffset || document.documentElement.scrollTop;
+  var scroll = this.pageYOffset || document.documentElement.scrollTop;
   var wrapper = document.getElementById('page-wrapper'); 
   var dynamic = document.getElementById('nav-bar');
   var height = wrapper.offsetHeight;
@@ -50,4 +50,22 @@ window.addEventListener('scroll', function(){
     nav2.style.transition = '1s ease-out';
     nav3.style.transition = '1s ease-out';
   }
+
+      const mediaQuery = window.matchMedia('(max-width: 650px)')
+
+if(mediaQuery.matches && scroll <= 0) {
+  header.style.webkitBoxShadow = '0 0px 0px -0px black';
+	header.style.mozBoxShadow = '0 0px 0px -0px black';
+	header.style.boxShadow = '0 0px 0px -0px black';
+  header.style.transition = '0.3s ease-out';
+}
+else if (mediaQuery.matches && scroll > 0) {
+  header.style.webkitBoxShadow = '0 6px 8px -7px black';
+	header.style.mozBoxShadow = '0 6px 8px -7px black';
+	header.style.boxShadow = '0 6px 8px -7px black';
+  header.style.transition = '1s ease-out';
+}                 
+                       
 });
+
+
