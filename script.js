4fb2a55ef9a6cfe83466159ed0c9d9d4e4839183
DaCapoStotel <106014492+DaCapoStotel@users.noreply.github.com>
2022-05-29 10:25:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ window.addEventListener('scroll', function(){
     nav1.style.backgroundColor = "#ff5757";
     nav2.style.backgroundColor = "white";
     nav3.style.backgroundColor = "white";
-    nav1.style.transition = '1s ease-out';
-    nav2.style.transition = '1s ease-out';
-    nav3.style.transition = '1s ease-out';
+    nav1.style.transition = '0.8s ease-out';
+    nav2.style.transition = '0.8s ease-out';
+    nav3.style.transition = '0.8s ease-out';
   } else if (scroll <= 0) {
     nav1.style.color = "#ff5757";
     nav2.style.color = "#ff5757";
@@ -24,9 +24,9 @@ window.addEventListener('scroll', function(){
     nav1.style.backgroundColor = "white";
     nav2.style.backgroundColor = "white";
     nav3.style.backgroundColor = "white";
-    nav1.style.transition = '1s ease-out';
-    nav2.style.transition = '1s ease-out';
-    nav3.style.transition = '1s ease-out';
+    nav1.style.transition = '0.8s ease-out';
+    nav2.style.transition = '0.8s ease-out';
+    nav3.style.transition = '0.8s ease-out';
   }  
   else if (scroll > 800 && scroll < 1200) {
     nav1.style.color = "#ff5757";
@@ -35,9 +35,9 @@ window.addEventListener('scroll', function(){
     nav1.style.backgroundColor = "white";
     nav2.style.backgroundColor = "#ff5757";
     nav3.style.backgroundColor = "white";
-    nav1.style.transition = '1s ease-out';
-    nav2.style.transition = '1s ease-out';
-    nav3.style.transition = '1s ease-out';
+    nav1.style.transition = '0.8s ease-out';
+    nav2.style.transition = '0.8s ease-out';
+    nav3.style.transition = '0.8s ease-out';
   } 
   else if (scroll > 1200 && scroll < 1500) {
     nav1.style.color = "#ff5757";
@@ -46,9 +46,9 @@ window.addEventListener('scroll', function(){
     nav1.style.backgroundColor = "white";
     nav2.style.backgroundColor = "white";
     nav3.style.backgroundColor = "rgba(255,87,87,1)";
-    nav1.style.transition = '1s ease-out';
-    nav2.style.transition = '1s ease-out';
-    nav3.style.transition = '1s ease-out';
+    nav1.style.transition = '0.8s ease-out';
+    nav2.style.transition = '0.8s ease-out';
+    nav3.style.transition = '0.8s ease-out';
   }
 
       const mediaQuery = window.matchMedia('(max-width: 650px)')
@@ -64,8 +64,24 @@ else if (mediaQuery.matches && scroll > 0) {
 	header.style.mozBoxShadow = '0 6px 8px -7px black';
 	header.style.boxShadow = '0 6px 8px -7px black';
   header.style.transition = '1s ease-out';
-}                 
+}       
+
+  
+  
+if (mediaQuery.matches && scroll > 1400) {
+  details.style.fontSize = '1.3rem';
+}
+else if (mediaQuery.matches && scroll < 1400) {
+  details.style.fontSize = '1.1rem';
+  }  
+else if (/*!(mediaQuery.matches && */scroll > 1200) {
+  details.style.fontSize = '1.6rem';
+}    
+else if (/*!(mediaQuery.matches &&*/ scroll < 1200) {
+  details.style.fontSize = '1.4rem';
+}
+
+  
                        
 });
-
 
