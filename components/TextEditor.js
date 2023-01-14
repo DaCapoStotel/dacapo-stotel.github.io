6324fa508b058ed6e95695c14e50e9643a740c86
DaCapoStotel <106014492+DaCapoStotel@users.noreply.github.com>
2023-01-14 15:24:13 +0100

    Update TextEditor.js

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,1 +1,12 @@
+import React, { useState } from 'react';
 
+const TextEditor = () => {
+  const [text, setText] = useState('Initial Text');
+  return (
+    <div>
+      <input type="text" value={text} onChange={e => setText(e.target.value)} />
+      <p>{text}</p>
+    </div>
+  );
+}
+export default TextEditor;
